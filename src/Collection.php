29f341d22bab5431a2af7f08e0212cb456b0c844
Nathan Daly <nathan.daly@harnessproperty.com>
2018-08-15 13:32:07 +0100

    Added Json Seriazable interface so that the collection cen be used with the \json_encode function. Added ext-json to composer for UX.

--- src/Collection.php
+++ src/Collection.php
@@ -18,7 +18,6 @@
 
 namespace Somnambulist\Collection;
 
-use function foo\func;
 use Somnambulist\Collection\Interfaces\ExportableInterface;
 use Somnambulist\Collection\Traits;
 use Somnambulist\Collection\Utils\Support;
@@ -30,13 +29,14 @@ use Somnambulist\Collection\Utils\Support;
  * @subpackage Somnambulist\Collection\Collection
  * @author     Dave Redfern
  */
-class Collection implements \ArrayAccess, \Countable, \IteratorAggregate, \Serializable, ExportableInterface
+class Collection implements \ArrayAccess, \Countable, \IteratorAggregate, \Serializable, \JsonSerializable, ExportableInterface
 {
 
     use Traits\ArrayAccess;
     use Traits\Exportable;
     use Traits\MagicMethods;
     use Traits\Serializable;
+    use Traits\JsonSerializable;
     use Traits\Sortable;
 
     /**
